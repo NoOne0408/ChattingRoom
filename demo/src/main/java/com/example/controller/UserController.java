39e858d4b9a9f71725e281ad13ed087d9c886db8
NoOne0408 <56995760+NoOne0408@users.noreply.github.com>
2021-04-22 02:37:23 +0800

    'try'

--- demo/src/main/java/com/example/controller/UserController.java
+++ demo/src/main/java/com/example/controller/UserController.java
@@ -22,13 +22,14 @@ public class UserController {
 
     @PostMapping("/login")
     public Result<User> login(@RequestBody User user) {
-        if (!checkParam(user)) {
+        if (!checkParam(user)) {//如果有参数未填写，返回错误
             return Result.error("-1", "缺少必要参数");
         }
         User dbUser = userMapper.selectByUsernameAndPassword(user);
-        if (dbUser == null) {
+        if (dbUser == null) {//如果通过账号和密码无法找到对应的用户
             return Result.error("-1", "账号或密码错误");
         }
+        //如果可以查到对应的用户则返回成功
         return Result.success(dbUser);
     }
 
