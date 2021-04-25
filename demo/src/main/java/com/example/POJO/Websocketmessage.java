8979fe92c89cb960b6324954d363392fcd9d08fc
luoyusk <2213800684@qq.com>
2021-04-25 22:58:49 +0800

    细节完善

--- demo/src/main/java/com/example/POJO/Websocketmessage.java
+++ demo/src/main/java/com/example/POJO/Websocketmessage.java
@@ -1,64 +1,88 @@
 package com.example.POJO;
 
 /*enum Messagetype{
-    openseesion,communication,groupcommunication,pub
+    Communication           用户间聊天
+    GroupCommunication      群聊
+    Pub                     匿名聊天公开身份
+                            动态
+    Success                 传输成功返回消息
+    Error                   传输失败返回消息
+    AddFriend               添加好友
+    AddGroup                加入群聊
+    CreateGroup             创建群聊
+    Function                功能型消息
+    UnNamedMatch            匿名聊天申请
+    TopicMatch              话题匹配
 }
 */
+
+//contexttype用区分传送信息是文字还是文件，依次为text,file
+
+/*enum contexttype{
+    Text                    通信时传输文本
+    File                    通信时传输图片
+    AddFriendSuccess        申请添加好友成功
+    AddGroupSuccess         申请加入群聊成功
+    CreateGroupSuccess      创建群聊
+    MatchSuccess            匹配成功
+}
+*/
+
 public class Websocketmessage implements java.io.Serializable {
     private String messagetype;
     private String touserid;
-    private String openedsession;
     private String message;
     private String fromuserid;
-
-    public String getMessagetype() {
-        return messagetype;
-    }
+    private String contexttype;
 
     @Override
     public String toString() {
         return "Websocketmessage{" +
                 "messagetype='" + messagetype + '\'' +
                 ", touserid='" + touserid + '\'' +
-                ", openedsession='" + openedsession + '\'' +
                 ", message='" + message + '\'' +
                 ", fromuserid='" + fromuserid + '\'' +
+                ", contexttype='" + contexttype + '\'' +
                 '}';
     }
 
-    public String getTouserid() {
-        return touserid;
-    }
-
-    public String getOpenedsession() {
-        return openedsession;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getFromuserid() {
-        return fromuserid;
+    public String getMessagetype() {
+        return messagetype;
     }
 
     public void setMessagetype(String messagetype) {
         this.messagetype = messagetype;
     }
 
+    public String getTouserid() {
+        return touserid;
+    }
+
     public void setTouserid(String touserid) {
         this.touserid = touserid;
     }
 
-    public void setOpenedsession(String openedsession) {
-        this.openedsession = openedsession;
+    public String getMessage() {
+        return message;
     }
 
     public void setMessage(String message) {
         this.message = message;
     }
 
+    public String getFromuserid() {
+        return fromuserid;
+    }
+
     public void setFromuserid(String fromuserid) {
         this.fromuserid = fromuserid;
+    }
+
+    public String getContexttype() {
+        return contexttype;
+    }
+
+    public void setContexttype(String contexttype) {
+        this.contexttype = contexttype;
     }
 }
--- demo/src/main/java/com/example/controller/WebSocket.java
+++ demo/src/main/java/com/example/controller/WebSocket.java
@@ -26,7 +26,134 @@ public class WebSocket {
     public void onMessage(Session session, String message) throws IOException {
         System.out.println("客户端发送："+message);
         Websocketmessage wb = JSON.parseObject(message,Websocketmessage.class);
-        send(this.userid,wb);
+        Websocketmessage swb = new Websocketmessage();
+        //用户间通信
+        switch (wb.getMessagetype()) {
+            case "Communication":
+                swb.setMessagetype(wb.getMessagetype());
+                swb.setContexttype(wb.getContexttype());
+                swb.setFromuserid(this.userid);
+                swb.setMessage(wb.getMessage());
+                //用户发送了一个文本消息
+                switch (wb.getContexttype()) {
+                    case ("Text"):
+                        //将该消息传入数据库
+
+                        break;
+                    //用户发送了一个图片消息
+                    case "File":
+                        //将该消息存入数据库
+
+                        break;
+                    default:
+                        swb = new Websocketmessage();
+                        swb.setMessagetype("Error");
+                        senderror(swb);
+                        return;
+                }
+                send(wb.getTouserid(), swb);
+                break;
+            //群聊通信
+            case "GroupCommunication":
+                swb.setMessagetype(wb.getMessagetype());
+                swb.setContexttype(wb.getContexttype());
+                swb.setFromuserid(this.userid);
+                swb.setMessage(wb.getMessage());
+                switch (wb.getContexttype()) {
+                    case "Text":
+                        //将该消息传入数据库
+                        break;
+                    case "File":
+                        //将该消息传入数据库
+                        break;
+                    default:
+                        swb = new Websocketmessage();
+                        swb.setMessagetype("Error");
+                        senderror(swb);
+                        return;
+                }
+                send(wb.getTouserid(), swb);
+                break;
+            //添加好友申请
+            case "AddFriend":
+                swb.setMessagetype(wb.getMessagetype());
+                swb.setFromuserid(this.userid);
+                send(wb.getTouserid(), swb);
+                break;
+            //加入群聊
+            case "AddGroup":
+                //将该消息存入数据库(?)
+                swb.setMessagetype(wb.getMessagetype());
+                swb.setFromuserid(this.userid);
+                //从数据库中查的群聊的群主
+                String id = "";
+                send(id, swb);
+                break;
+            //创建群聊
+            case "CreateGroup":
+                //向数据库建立群聊
+                send(this.userid, swb);
+                break;
+            //匿名匹配聊天
+            case "UnNamedMatch":
+                //调用函数（很麻烦）
+                send(this.userid, swb);
+                break;
+            //话题匹配聊天
+            case "TopicMatch":
+                //调用函数（很麻烦）
+                send(this.userid, swb);
+                break;
+            //公开身份
+            case "Pub":
+                swb.setMessagetype(wb.getMessagetype());
+                swb.setFromuserid(this.userid);
+                send(wb.getTouserid(), swb);
+
+                break;
+            //功能型消息
+            case "Fuction":
+                //添加好友成功响应
+                switch (wb.getContexttype()) {
+                    case "AddFriendSuccess":
+                        //将该消息存入数据库
+
+                        swb.setMessagetype(wb.getMessagetype());
+                        swb.setContexttype(wb.getContexttype());
+                        swb.setFromuserid(this.userid);
+                        send(wb.getTouserid(), swb);
+                        break;
+                    //添加好友失败响应
+                    case "AddFriendFailed":
+                        //将该消息存入数据库（？）
+
+                        swb.setMessagetype(wb.getMessagetype());
+                        swb.setContexttype(wb.getContexttype());
+                        swb.setFromuserid(this.userid);
+                        send(wb.getTouserid(), swb);
+                        break;
+                    //入群成功响应
+                    case "AddGroupSuccess":
+                        //将该消息存入数据库
+
+                        swb.setMessagetype(wb.getMessagetype());
+                        swb.setContexttype(wb.getContexttype());
+                        swb.setFromuserid(this.userid);
+                        send(wb.getTouserid(), swb);
+                        break;
+                    //入群失败响应
+                    case "AddGroupFailed":
+                        //将该消息存入数据库（？）
+
+                        swb.setMessagetype(wb.getMessagetype());
+                        swb.setContexttype(wb.getContexttype());
+                        swb.setFromuserid(wb.getFromuserid());
+                        send(wb.getTouserid(), swb);
+                        break;
+                }
+                break;
+        }
+
 
     }
     @OnOpen
@@ -46,6 +173,10 @@ public class WebSocket {
         if(session!=null && session.isOpen()){
             session.getAsyncRemote().sendObject(message);
         }
+    }
+    public void senderror(Websocketmessage message){
+        Session session = connections.get(this.userid);
+        session.getAsyncRemote().sendObject(message);
     }
 
 
